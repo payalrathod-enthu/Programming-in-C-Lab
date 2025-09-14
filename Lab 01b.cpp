fe54d966277664f3e69b7373b9889b4a7664046d
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2025-09-14 17:38:54 +0530

    Lab 1 Question 2

---
+++ Lab 01b.cpp
@@ -0,0 +1,9 @@
+//Write a C Program to print the address in multiple lines (new line). At least 4 lines, use \n and only one "printf" function.
+
+#include<stdio.h>
+
+int main(){
+	printf("Girls Scholar Hostel \n Near Walk in Bidholi \n UPES road \n Dehradun \n Uttarakhand");
+	return 0;
+}
+
